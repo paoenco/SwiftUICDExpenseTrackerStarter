31aa5344941771701e36f7debffe875cdf2c86dd
Paolo Encomienda <paoenco@gmail.com>
2023-01-26 11:55:44 -0600

    Inject managed object context to root view using SwiftUI Environment
    So all child views can receive this

--- ExpenseTracker/SceneDelegate.swift
+++ ExpenseTracker/SceneDelegate.swift
@@ -13,9 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
+    let coreDataStack = CoreDataStack(containerName: "ExpenseTracker")
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         let contentView = ContentView()
+            .environment(\.managedObjectContext, coreDataStack.viewContext)
 
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
