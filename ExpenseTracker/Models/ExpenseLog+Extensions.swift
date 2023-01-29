9c747414931c0ff8285f0e26e268b5c1be4eb56e
Paolo Encomienda <paoenco@gmail.com>
2023-01-28 18:07:30 -0600

    Add fetchAllCategoriesTotalAmountSum function

--- ExpenseTracker/Models/ExpenseLog+Extensions.swift
+++ ExpenseTracker/Models/ExpenseLog+Extensions.swift
@@ -38,4 +38,42 @@ extension ExpenseLog {
             return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
         }
     }
+    
+    static func fetchAllCategoriesTotalAmountSum(context: NSManagedObjectContext, completion: @escaping ([(sum: Double, category: Category)]) -> ()) {
+        
+            let keypathAmount = NSExpression(forKeyPath: \ExpenseLog.amount)
+            let expression = NSExpression(forFunction: "sum:", arguments: [keypathAmount])
+            
+            let sumDesc = NSExpressionDescription()
+            sumDesc.expression = expression
+            sumDesc.name = "sum"
+            sumDesc.expressionResultType = .decimalAttributeType
+            
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: ExpenseLog.entity().name ?? "ExpenseLog")
+            request.returnsObjectsAsFaults = false
+            request.propertiesToGroupBy = ["category"]
+            request.propertiesToFetch = [sumDesc, "category"]
+            request.resultType = .dictionaryResultType
+            
+            context.perform {
+                do {
+                    let results = try request.execute()
+                    let data = results.map { (result) -> (Double, Category)? in
+                        guard
+                            let resultDict = result as? [String: Any],
+                            let amount = resultDict["sum"] as? Double, amount > 0,
+                            let categoryKey = resultDict["category"] as? String,
+                            let category = Category(rawValue: categoryKey) else {
+                                return nil
+                        }
+                        return (amount, category)
+                    }.compactMap { $0 }
+                    completion(data)
+                } catch let error as NSError {
+                    print((error.localizedDescription))
+                    completion([])
+                }
+            }
+            
+        }
 }
